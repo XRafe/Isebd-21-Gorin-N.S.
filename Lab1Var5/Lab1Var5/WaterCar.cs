73f5f24f91ceb07c53c47a29f042383b4fb331ce
XRafe <nikita-gorin98@yandex.ru>
2018-11-17 01:57:02 +0400

    1

--- Lab1Var5/Lab1Var5/WaterCar.cs
+++ Lab1Var5/Lab1Var5/WaterCar.cs
@@ -10,9 +10,8 @@ namespace Lab1Var5
 {
     public class WaterCar : Ship
     {
-        /// <summary>
-        /// Дополнительный цвет
-        /// </summary>
+      
+
         public Color DopColor { private set; get; }
 
         public bool Wheel { private set; get; }
