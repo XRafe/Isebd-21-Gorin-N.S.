29bc9868837ff83f8fd7cf87a3f010a7f5ad4396
XRafe <43075627+XRafe@users.noreply.github.com>
2019-01-17 16:09:41 +0300

    Update FormParking.Designer.cs
    ch

--- Lab1Var5/Lab1Var5/FormParking.Designer.cs
+++ Lab1Var5/Lab1Var5/FormParking.Designer.cs
@@ -32,7 +32,7 @@
             this.загрузитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Sort = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxParking)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakeShip)).BeginInit();
             this.menuStrip1.SuspendLayout();
@@ -151,22 +151,22 @@
             // 
             this.saveFileDialog.Filter = "txt file | *.txt";
             // 
-            // button1
+            // Sort
             // 
-            this.button1.Location = new System.Drawing.Point(578, 174);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(173, 50);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "Сортировать";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Sort.Location = new System.Drawing.Point(578, 174);
+            this.Sort.Name = "Sort";
+            this.Sort.Size = new System.Drawing.Size(173, 50);
+            this.Sort.TabIndex = 11;
+            this.Sort.Text = "Сортировать";
+            this.Sort.UseVisualStyleBackColor = true;
+            this.Sort.Click += new System.EventHandler(this.Sort_Click);
             // 
             // FormParking
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(763, 468);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Sort);
             this.Controls.Add(this.buttonSetShip);
             this.Controls.Add(this.listBoxLevels);
             this.Controls.Add(this.label2);
@@ -189,7 +189,6 @@
         }
 
         #endregion
-
 
         private System.Windows.Forms.PictureBox pictureBoxParking;
 
@@ -219,6 +218,6 @@
 
         private System.Windows.Forms.SaveFileDialog saveFileDialog;
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Sort;
     }
 }
